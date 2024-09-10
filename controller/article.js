2a3af744322a7b7124c48d0413ed57581ca5f40a
J3alou5 <karliskade@gmail.com>
2024-09-10 14:21:58 +0300

    Displayed author article list using the author and article oop model

--- controller/article.js
+++ controller/article.js
@@ -1,19 +1,21 @@
-const articleDbModel = require('../models/article')
+const articleDbModel = require ('../models/article')
 const articleModel = new articleDbModel();
 
-class artcileController {
-  constructor() {
-    const articles = []
+class articleController {
+    constructor() {
+        const articles = []
+    }
+    
+    async getAllArticles(req, res){
+        const articles = await articleModel.findAll()
+        res.status(201).json({articles: articles})
+    }
+
+    async getArticleBySlug(req,res){
+        const article = await articleModel.findOne(req.params.slug)
+        res.status(201).json({article: article})
+    }
+    
   }
 
-  async getAllArticles(req, res) {
-    const articles = await articleModel.findAll()
-    res.status(201).json({articles: articles })
-  }
-  async getArticleBySlug(req, res){
-    const article = await articleModel.findOne(req.params.slug)
-    res.status(201).json({article: article})
-  }
-}
-
-module.exports = artcileController
+module.exports = articleController;
--- models/article.js
+++ models/article.js
@@ -1,16 +1,24 @@
 const BaseSQLModel = require('./base');
 
 class ArticleModel extends BaseSQLModel {
-    constructor() {
-        super('article');
-    }
-    async findAll(){
-        const articles = await super.findAll()
-        return articles 
-    }
-    async findOne(slug){
-        const article = await super.findOne('slug', slug)
-        return article
-    }
+  constructor() {
+    super('article');
+  }
+
+  async findAll(){
+    const articles = await super.findAll()
+    return articles
+  }
+
+  async findOne(slug){
+    const article = await super.findOne('slug', slug)
+    return article
+  }
+  async findMany(author){
+    const articles = await super.findMany('author_id', author.id)
+    return articles
+  }
 }
+
 module.exports = ArticleModel;
+
