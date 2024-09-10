64ac15f5716dbe0861fafac48f603049d4b9d5de
J3alou5 <karliskade@gmail.com>
2024-09-10 13:23:50 +0300

    slugs try

--- controller/article.js
+++ controller/article.js
@@ -10,6 +10,10 @@ class artcileController {
     const articles = await articleModel.findAll()
     res.status(201).json({ articles: articles })
   }
+  async getArticleBySlug(req, res){
+    const article = await articleModel.findOne(req.parms.slug)
+    res.status(201).json({article: article})
+  }
 }
 
 module.exports = artcileController
--- routes/articles.js
+++ routes/articles.js
@@ -4,6 +4,7 @@ const artcileControllerClass = require('../controller/article');
 
 const artcileController = new artcileControllerClass()
 
+router.get('/article/:slug', (req, res) => artcileController.getArticleBySlug(req, res));
 router.get('/', (req, res)=> artcileController.getAllArticles(req, res));
 
 module.exports = router;
